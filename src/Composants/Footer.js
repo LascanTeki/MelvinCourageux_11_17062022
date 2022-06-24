797dd4cf9f94a468bb1690a8db815eaac20881dc
LascanTeki <91802117+LascanTeki@users.noreply.github.com>
2022-06-24 03:19:54 +0200

    better footer

--- src/Composants/Footer.js
+++ src/Composants/Footer.js
@@ -4,10 +4,10 @@ import logo from '../Assets/footer.jpg'
 function Footer() {
     
     return (
-        <div className='Footer'>
+        <footer>
                  <img src={logo} alt='Kasa' className='foot' />
                  <div className='copyright'>© 2020 Kasa. All rights reserved</div>
-        </div>
+        </footer>
     )
 }
 
