3406e551f3588438604e814619de1ae6524895f4
LascanTeki <91802117+LascanTeki@users.noreply.github.com>
2022-06-27 19:41:25 +0200

    ERROR 404 on fiche handling

--- src/Pages/Fiche.js
+++ src/Pages/Fiche.js
@@ -2,6 +2,7 @@ import Gallery from '../Composants/Gallery'
 import Description from '../Composants/Description'
 import list from '../Composants/List'
 import Deroulant from '../Composants/Deroulant';
+import { Navigate } from 'react-router-dom';
 
 function filterphoto(value, list) {
     return list.filter(d => d.id === value);
@@ -17,11 +18,18 @@ function Fiche() {
 
 
     let newlist = filterphoto(value, list);
+
+    if (newlist[0]===undefined) {
+        return <Navigate to='/404'  />
+    }
+
     let eq = newlist[0].equipments
 
     let equipement =(<ul> {eq.map((eq) => (
         <li key={eq}>{eq}</li>
     ))}</ul>)
+
+        
 
     return (
         <div><Gallery newlist={newlist}/><Description newlist={newlist}/><div className='flexed'><Deroulant content={newlist[0].description} title="Description" /><Deroulant content={equipement} title="Equipements" /></div></div>
